f17b3be192fcf792b9a56aa76cabf5fb80497822
Ben <benharvie94@gmail.com>
2018-10-30 17:29:36 +0000

    Added Song.js and exports

---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ChartContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      songs: []
+    }
+  }
+}
+
+export default ChartContainer
